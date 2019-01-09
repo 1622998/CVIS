57f3487f8bd2c29df4973bc0acc4de31ac7f2921
Ben <1622998@stud.hs-mannheim.de>
2019-01-09 14:23:53 +0100

    Aufgabe 13 etwas optimiert

--- 13_Uebungsblatt/13_OptischerFluss.py
+++ 13_Uebungsblatt/13_OptischerFluss.py
@@ -11,41 +11,43 @@ print "Übungsblatt 13"
 print "--------------"
 print "Optischer Fluss"
 
+source_directory = 'images/'
+destination_directory = 'OpticalFlows/'
+pair_name = '000042_'
+ending = '.png'
 
-def bildpaar_einlesen(path, pair_name, ending):
-    print "1 *lese Bilder ein*"
-    pics = glob.glob(path + pair_name + "*" + ending)
+print "1. *lese Bilder ein*"
+pics = glob.glob(source_directory + pair_name + "*" + ending)
 
-    print "2 *berechne optischen Fluss für ", len(pics), " Bilder*"
-
-    def optischen_fluss_berechnen(first0, second0):
-        """
-        Berechnet den optischen Fluss zwischen zwei Bildern
-        :return: Farbbild mit optischen Fluss
-        """
-        first = cv2.imread(first0, 0)
-        second = cv2.imread(second0, 0)
-        flow = cv2.calcOpticalFlowFarneback(first, second, None, 0.5, 3, 20, 3, 5, 1.2, 0)
-
-        mag, ang = cv2.cartToPolar(flow[..., 0], flow[..., 1])
-        hsv = np.zeros((first.shape[0], first.shape[1], 3))
-        hsv[..., 0] = ang * 180 / np.pi / 2
-        hsv[..., 1] = 255
-        hsv[..., 2] = cv2.normalize(mag, None, 0, 255, cv2.NORM_MINMAX)
-        hsv = hsv.astype(np.uint8)
-        bgr0 = cv2.cvtColor(hsv, cv2.COLOR_HSV2BGR)
-
-        return bgr0
-
-    x = 0
-    while x < len(pics) - 1:
-        bgr = optischen_fluss_berechnen(pics[x], pics[x + 1])
-        bildname = 'OptischerFluss_' + pair_name + str(1 + x) + ending
-        cv2.imwrite(bildname, bgr)
-        print "*", bildname, " gespeichert*"
-        x = x + 1
+print "2. *berechne optischen Fluss für ", len(pics), " Bilder*"
 
 
-# [START] Aufruf der Funktion
-bildpaar_einlesen('images/', '000042_', '.png')
+def optischen_fluss_berechnen(first0, second0):
+    """
+    Berechnet den optischen Fluss zwischen zwei Bildern
+    :return: Farbbild mit optischen Fluss
+    """
+    first = cv2.imread(first0, 0)
+    second = cv2.imread(second0, 0)
+    flow = cv2.calcOpticalFlowFarneback(first, second, None, 0.5, 3, 20, 3, 5, 1.2, 0)
+
+    mag, ang = cv2.cartToPolar(flow[..., 0], flow[..., 1])
+    hsv = np.zeros((first.shape[0], first.shape[1], 3))
+    hsv[..., 0] = ang * 180 / np.pi / 2
+    hsv[..., 1] = 255
+    hsv[..., 2] = cv2.normalize(mag, None, 0, 255, cv2.NORM_MINMAX)
+    hsv = hsv.astype(np.uint8)
+    bgr0 = cv2.cvtColor(hsv, cv2.COLOR_HSV2BGR)
+
+    return bgr0
+
+
+x = 0
+while x < len(pics) - 1:
+    bgr = optischen_fluss_berechnen(pics[x], pics[x + 1])
+    bildname = 'Optischer_Fluss_zwischen_' + pair_name + str(x) + '_und_' + str(1 + x) + ending
+    cv2.imwrite(destination_directory + bildname, bgr)
+    print "3. *", bildname, " in ", destination_directory, "gespeichert*"
+    x = x + 1
+
 print("[ENDE]")
